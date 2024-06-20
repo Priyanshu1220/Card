70567c31e444fc5ff51acc6ee6bb23bea3188a43
Priyanshu1220 <166621477+Priyanshu1220@users.noreply.github.com>
2024-06-20 11:53:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// let imgbox=document.getElementById('body');
+// imgbox.addEventListener('mouseover',function handlemouseout(){
+//     imgbox.style.backgroundImage="url(https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.freepik.com%2Fpremium-photo%2Fabstract-background-with-tropical-leaves-magenta-azure-generative-ai_836899-686.jpg&tbnid=hmr8e-antuorXM&vet=12ahUKEwjBysrR-JiFAxVke2wGHWSVBY4QMygIegUIARCAAQ..i&imgrefurl=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fhd-background&docid=5VSocAbMiJ7CdM&w=626&h=417&q=background%20image&ved=2ahUKEwjBysrR-JiFAxVke2wGHWSVBY4QMygIegUIARCAAQ)"
+// });
